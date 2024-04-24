f07c12af548c2f566f2258bcdfc0799b9889409b
Matheus-Merlos <matheusazrmerlos@gmail.com>
2024-04-24 09:51:41 -0300

    Matheus - 24/04 - Terminei os Scripts de geração dos dados

--- criar_dados.py
+++ criar_dados.py
@@ -3,6 +3,7 @@ import os
 import random
 from abc import ABC, abstractmethod
 from pathlib import Path
+from typing import Tuple
 
 PATH = Path(__file__).parent
 
@@ -16,7 +17,7 @@ def save_to_file(func):
 
 
 class AbstractGenerator(ABC):
-    def __init__(self, seed: int, language: str, taxi_path: str, driver_path: str, user_path: str, course_path: str) -> None:
+    def __init__(self, seed: int, language: str, taxi_path: str, driver_path: str, user_path: str, course_path: str, enterprises_path: str, place_path: str, queue_path: str) -> None:
         super().__init__()
         
         files = [file.lower() for file in os.listdir(PATH) if file.endswith('.sql')]
@@ -31,18 +32,24 @@ class AbstractGenerator(ABC):
         self._driver_path = driver_path
         self._user_path = user_path
         self._course_path = course_path
+        self._enterprises_path = enterprises_path
+        self._place_path = place_path
+        self._queue_path = queue_path
 
 
 class Generator(AbstractGenerator):
-    def __init__(self, seed: int, language: str, taxi_path: str, driver_path: str, user_path: str, course_path) -> None:
-        super().__init__(seed, language, taxi_path, driver_path, user_path, course_path)
+    def __init__(self, seed: int, language: str, taxi_path: str, driver_path: str, user_path: str, course_path, enterprises_path: str, place_path: str, queue_path: str) -> None:
+        super().__init__(seed, language, taxi_path, driver_path, user_path, course_path, enterprises_path, place_path, queue_path)
         self.__plates = list()
         self.__cnhs = list()
         self.__user_quantity = 0
+        self.__cpfs = []
+        self.__cnpjs = []
+        self.__place_quantity = 0
     
     
     @save_to_file
-    def generate_fake_taxis(self, quantity: int) -> None:
+    def generate_fake_taxis(self, quantity: int) -> Tuple[str, str]:
         number = 0
         file_as_str = ''
         
@@ -53,7 +60,7 @@ class Generator(AbstractGenerator):
         ]
         
         for i in range(0, quantity):
-            placa = f'AAA-{str(number).zfill(4)}'
+            placa = f'AAA{str(number).zfill(4)}'
             self.__plates.append(placa)
             
             brand = random.choice(car_brands)
@@ -68,7 +75,7 @@ class Generator(AbstractGenerator):
         return self._taxi_path, file_as_str
     
     @save_to_file
-    def generate_fake_drivers(self, quantity: int) -> None:        
+    def generate_fake_drivers(self, quantity: int) -> Tuple[str, str]:        
         file_as_str = ''
     
         for i in range(0, quantity):
@@ -82,23 +89,50 @@ class Generator(AbstractGenerator):
             
         return self._driver_path, file_as_str
     
+    
+    @staticmethod
+    def __generate_until_unique(function, lst: list) -> int:
+        generated_value = function()
+        while generated_value in lst:
+            generated_value = function()
+        
+        lst.append(generated_value)
+        return generated_value
+    
+    
     @save_to_file
-    def generate_fake_users(self, quantity: int) -> None:
-        self.__user_quantity = quantity
-
+    def generate_fake_users(self, quantity: int) -> Tuple[str, str]:
+        self.__user_quantity += quantity
         
         file_as_str = ''
     
         for i in range(0, quantity):
             name = self._faker.name()
-            cpf = self._faker.cpf()
+            cpf = self.__generate_until_unique(self._faker.cpf, self.__cpfs)
             
-            file_as_str += (f"INSERT INTO Cliente(CliId, Nome, CPF) VALUES ('{i}', '{name}', {cpf});\n")
+            file_as_str += (f"INSERT INTO ClienteFisico(CliId, Nome, CPF) VALUES ('{i}', '{name}', '{cpf}');\n")
             
         return self._user_path, file_as_str
     
+    
     @save_to_file
-    def generate_fake_drives(self, quantity: int) -> None:    
+    def generate_fake_enterprises(self, quantity: int) -> Tuple[str, str]:
+        self.__user_quantity += quantity
+        file_as_str = ''
+        
+        for i in range(0, quantity):
+            name = self._faker.name()
+            cnpj = self._faker.cnpj()
+            
+            cnpj = self.__generate_until_unique(self._faker.cnpj, self.__cnpjs)
+            
+            file_as_str += (f"INSERT INTO ClienteEmpresa(CliId, Nome, CNPJ) VALUES ('{i}', '{name}', '{cnpj}');\n")
+        
+        return self._enterprises_path, file_as_str
+    
+    
+    @save_to_file
+    def generate_fake_drives(self, quantity: int) -> Tuple[str, str]:    
         file_as_str = ''
         for i in range(0, quantity):
             user_id = random.randint(0, self.__user_quantity)
@@ -108,12 +142,45 @@ class Generator(AbstractGenerator):
             file_as_str += (f"INSERT INTO Corrida(CliId, Placa, DataPedido) VALUES ('{user_id}', '{plate}', '{date}');\n")
         
         return self._course_path, file_as_str
+    
+    
+    @save_to_file
+    def generate_fake_places(self, quantity: int) -> Tuple[str, str]:
+        file_as_str = ''
+        self.__place_quantity = quantity
+        
+        for i in range(0, quantity):
+            name = self._faker.city()
+            
+            file_as_str += (f"INSERT INTO Zona(Zona) VALUES ('{name}');\n")
+        
+        return self._place_path, file_as_str
+    
+    
+    @save_to_file
+    def generate_fake_queues(self, quantity: int) -> Tuple[str, str]:
+        file_as_str = ''
+        
+        for i in range(0, quantity):
+            zona = random.randint(1, self.__place_quantity)
+            cnh = random.choice(self.__cnhs)
+            data_hora_in = self._faker.date_time_this_decade()
+            data_hora_out = self._faker.date_time_this_decade()
+            km_in = random.randint(500, 120000)
+            
+            
+            file_as_str += (f"INSERT INTO Fila(Zona, CNH, DataHoraIn, DataHoraOut, KmIn) VALUES ('{zona}', '{cnh}', {data_hora_in}, {data_hora_out}, {km_in});\n")
+        
+        return self._queue_path, file_as_str
             
             
 if __name__ == '__main__':
-    fake_generator = Generator(1024, 'PT-BR', 'taxis.sql', 'motoristas.sql', 'clientes.sql', 'corridas.sql')
+    fake_generator = Generator(1024, 'PT-BR', 'taxis.sql', 'motoristas.sql', 'clientes.sql', 'corridas.sql', 'empresas.sql', 'zonas.sql', 'filas.sql')
     fake_generator.generate_fake_taxis(10000)
     fake_generator.generate_fake_drivers(10000)
-    fake_generator.generate_fake_users(19789)
+    fake_generator.generate_fake_users(10000)
+    fake_generator.generate_fake_enterprises(10000)
     fake_generator.generate_fake_drives(65535)
+    fake_generator.generate_fake_places(16355)
+    fake_generator.generate_fake_queues(32768)
     
