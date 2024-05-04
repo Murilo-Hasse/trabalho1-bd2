820a81cd0ee0fc6a6fc818830c7a7be9f7a6f3a2
Leonardo F. G. Santana <53916135+LeonardoSantan@users.noreply.github.com>
2024-05-04 14:06:17 -0300

    Add files via upload

---
+++ Selects/SELECT_1.sql
@@ -0,0 +1,17 @@
+--Necessito extrair uma informação do banco, do qual traga, a contagem de corridas e o total de km percorridos
+--por um de nossos taxistas do qual, traga filtro de zona(nome da zona), o periodo e o nome do taxista em questão.
+SELECT count(*) as Quantidade_de_corridas,SUM(COR.kmtotal) as Total_de_Kms_percorridos
+FROM CORRIDA AS COR
+
+	INNER JOIN MOTORISTA AS MOT
+		ON(COR.PLACA = MOT.PLACA)
+	INNER JOIN FILA
+		ON(MOT.CNH = FILA.CNH)
+	INNER JOIN zona
+		ON (zona.id = FILA.Zona)
+WHERE
+	zona.zona = 'Farias Grande'
+AND
+	COR.Datapedido between '01-01-1995' and '31-12-2022'
+AND
+	MOT.Nome = 'Bernardo Mendes'
