bae9b571b6be383fd62f557eca39a6e3b0014f1a
Matheus-Merlos <matheusazrmerlos@gmail.com>
2024-05-07 19:12:31 -0300

    Arrumando a ordem

--- graficos/graph_maker.py
+++ graficos/graph_maker.py
@@ -9,6 +9,7 @@ class PlotIndexGraph:
         self.__labels = labels
         
         assert len(color_pallette_hex) == len(labels), "O tamanho da lista de paleta de cores deve ser o mesmo tamanho da lista de labels"
+        ]
 
 
     def plot_and_show(self, values: list, graph_precision: int, title: str, font_size: int, x_label: str, y_label: str) -> None:
@@ -24,6 +25,7 @@ class PlotIndexGraph:
         bars = plt.bar(self.__labels, values)
         plt.yticks(y_ticks)
         
+        
         [bar.set_color(self.__color_pallette[idx]) for idx, bar in enumerate(bars)]
         
         plt.title(title, fontdict={'fontsize': font_size})
@@ -34,15 +36,15 @@ class PlotIndexGraph:
         plt.show()  
 
 if __name__ == '__main__':
-    labels = ['Sem Índice', 'HASH', 'BTREE', 'BRIN', 'GIN', 'GIST']
+    labels = ['Sem Índice', 'HASH', 'BTREE', 'GIN', 'GIST', 'BRIN']
     color_pallette = ['#1B51F2', '#1D79F2', '#22A2F2', '#F2CF1D', '#1D79F2', '#1B51F2']
     
     font_size = 32
     
     
     values_select_1 = [13.22, 13.14, 11.50, 13.86, 13.56, 15.18]
-    values_select_2 = [118.02, 432.94, 353.56, 381.22, 371.85, 371.56]
-    values_select_3 = [227.36, 234.17, 230.55, 224.25, 225.26, 223.69]
+    values_select_2 = [1.27, 1.53, 3.26, 0.57, 0.96, 1.60]
+    values_select_3 = [10.20, 12.17, 10.94, 12.33, 12.53, 12.60]
     values_select_4 = [18.40, 13.85, 14.00, 13.29, 13.13, 13.94]
     values_select_5 = [16.05, 17.44, 7.36, 19.96, 19.46, 20.74]
     
@@ -50,12 +52,12 @@ if __name__ == '__main__':
     graph_select_1 = PlotIndexGraph(color_pallette, labels)
     
     graph_select_1.plot_and_show(values=values_select_1, graph_precision=10, title='Tempo médio de execução SELECT 1', font_size=font_size, x_label='Tipo de Índice', y_label='Tempo médio (Em MS)')
-    graph_select_1.plot_and_show(values=values_select_2, graph_precision=125, title='Tempo médio de execução SELECT 2', font_size=font_size, x_label='Tipo de Índice', y_label='Tempo médio (Em MS)')
-    graph_select_1.plot_and_show(values=values_select_3, graph_precision=100, title='Tempo médio de execução SELECT 3', font_size=font_size, x_label='Tipo de Índice', y_label='Tempo médio (Em MS)')
+    graph_select_1.plot_and_show(values=values_select_2, graph_precision=5, title='Tempo médio de execução SELECT 2', font_size=font_size, x_label='Tipo de Índice', y_label='Tempo médio (Em MS)')
+    graph_select_1.plot_and_show(values=values_select_3, graph_precision=17, title='Tempo médio de execução SELECT 3', font_size=font_size, x_label='Tipo de Índice', y_label='Tempo médio (Em MS)')
     graph_select_1.plot_and_show(values=values_select_4, graph_precision=12, title='Tempo médio de execução SELECT 4', font_size=font_size, x_label='Tipo de Índice', y_label='Tempo médio (Em MS)')
     graph_select_1.plot_and_show(values=values_select_5, graph_precision=10, title='Tempo médio de execução SELECT 5', font_size=font_size, x_label='Tipo de Índice', y_label='Tempo médio (Em MS)')
         
-    labels = ['HASH', 'BTREE', 'BRIN', 'GIN', 'GIST']
+    labels = ['HASH', 'BTREE', 'GIN', 'GIST', 'BRIN']
     color_pallette = ['#1B51F2', '#1D79F2', '#22A2F2', '#F2CF1D', '#1D79F2']    
         
     graph_size_1 = [6889472, 2695168, 1695744, 4997120, 196608]
